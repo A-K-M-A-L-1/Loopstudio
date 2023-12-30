e10c105235e4d4690ec221d84a267a87815113f9
Akmal <akmalfaraz867@gmail.com>
2023-12-30 16:50:39 +0500

    inital commit

---
+++ Js/script.js
@@ -0,0 +1,13 @@
+let btn= document.getElementById('menu-btn');
+let menu = document.getElementById('menu');
+
+btn.addEventListener('click', navtoggle)
+
+function navtoggle(){
+
+    btn.classList.toggle('open')
+    menu.classList.toggle('flex')
+    menu.classList.toggle('hidden')
+}
+
+
